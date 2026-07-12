07075ccced865841589e686a69e829fce9991a82
Ercin Cakaloglu <cakalogluer@gmail.com>
2026-07-12 22:25:37 +0300

    Add prev/next arrow navigation to the fullscreen photo viewer

--- lib/features/requests/presentation/request_photo_gallery.dart
+++ lib/features/requests/presentation/request_photo_gallery.dart
@@ -44,7 +44,7 @@ class RequestPhotoGallery extends StatelessWidget {
   }
 }
 
-class _RequestPhotoViewerScreen extends StatelessWidget {
+class _RequestPhotoViewerScreen extends StatefulWidget {
   const _RequestPhotoViewerScreen({
     required this.photoUrls,
     required this.initialIndex,
@@ -54,16 +54,81 @@ class _RequestPhotoViewerScreen extends StatelessWidget {
   final int initialIndex;
 
   @override
+  State<_RequestPhotoViewerScreen> createState() =>
+      _RequestPhotoViewerScreenState();
+}
+
+class _RequestPhotoViewerScreenState extends State<_RequestPhotoViewerScreen> {
+  late final PageController _controller =
+      PageController(initialPage: widget.initialIndex);
+  late int _currentIndex = widget.initialIndex;
+
+  void _goTo(int index) {
+    _controller.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 250),
+      curve: Curves.easeOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(backgroundColor: Colors.black, foregroundColor: Colors.white),
-      body: PageView.builder(
-        controller: PageController(initialPage: initialIndex),
-        itemCount: photoUrls.length,
-        itemBuilder: (context, index) => InteractiveViewer(
-          child: Center(child: Image.network(photoUrls[index])),
-        ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          PageView.builder(
+            controller: _controller,
+            itemCount: widget.photoUrls.length,
+            onPageChanged: (index) => setState(() => _currentIndex = index),
+            itemBuilder: (context, index) => InteractiveViewer(
+              child: Center(child: Image.network(widget.photoUrls[index])),
+            ),
+          ),
+          if (_currentIndex > 0)
+            Positioned(
+              left: 8,
+              child: _NavArrowButton(
+                icon: Icons.chevron_left_rounded,
+                onPressed: () => _goTo(_currentIndex - 1),
+              ),
+            ),
+          if (_currentIndex < widget.photoUrls.length - 1)
+            Positioned(
+              right: 8,
+              child: _NavArrowButton(
+                icon: Icons.chevron_right_rounded,
+                onPressed: () => _goTo(_currentIndex + 1),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class _NavArrowButton extends StatelessWidget {
+  const _NavArrowButton({required this.icon, required this.onPressed});
+
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.black45,
+      shape: const CircleBorder(),
+      child: IconButton(
+        icon: Icon(icon, color: Colors.white, size: 32),
+        onPressed: onPressed,
       ),
     );
   }
